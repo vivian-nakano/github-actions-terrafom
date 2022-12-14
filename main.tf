ae468bf96d99ca3c2593b2b5b4e502b93b2a2a74
vivian-nakano <vrnakano@gmail.com>
2022-12-14 00:19:40 -0300

    insert terraform and gha structure

--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
 terraform {
 
-  required_version = ">= 1.0.0"
+    required_version = ">= 1.0.0"
 
-  required_providers {
+    required_providers {
 
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.73.0"
-    }
-  }
-
-  provider "aws" {
-    region = "eu-central-1"
-
-    default_tags {
-      tags = {
-        owner      = "viviannakano"
-        managed-by = "terraform"
+      aws = {
+        source  = "hashicorp/aws"
+        version = "3.73.0"
       }
     }
-  }
 
-  data "terraform_remote_state" "vpc" {
-    backend = "s3"
-    config = {
-      bucket = "vivian-remote-state"
-      key    = "aws-vpc/terraform.tfstate"
-      region = "sa-central-1"
+provider "aws" {
+  region = "eu-central-1"
+
+  default_tags {
+    tags = {
+      owner      = "viviannakano"
+      managed-by = "terraform"
+    }
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "vivian-remote-state"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "sa-central-1"
     }
   }
 }
